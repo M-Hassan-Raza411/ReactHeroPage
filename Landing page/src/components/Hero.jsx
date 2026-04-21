70881f3948a3ea4364aad29fbada0bf654558134
M Hassan Raza <mhassanraza896@gmail.com>
2026-04-21 22:20:15 +0500

    little change

--- Landing page/src/components/Hero.jsx
+++ Landing page/src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { PrimaryButton, SecondaryButton } from './Button'
 const Hero = () => {
   return (
     <div className='hero'>
-      <span className='heroinfo'>Hello, I'm John Doe</span>
+      <span className='heroinfo'>Hello, I'm  Hassan Raza</span>
       <h1 className='herotitle'>Full Stack Developer</h1>
       <p className='herodesc'>
         I build exceptional digital experiences that are fast, accessible, and visually appealing. With expertise in both front-end and back-end development, I create seamless and efficient web applications that meet the needs of users and businesses alike. Let's work together to bring your ideas to life!  
