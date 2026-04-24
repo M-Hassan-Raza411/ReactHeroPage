6456e91259c181f62b02b2161e9f74b9cb98f54a
M Hassan Raza <mhassanraza896@gmail.com>
2026-04-24 12:22:11 +0500

    Added Slider js and responsiveness

--- Landing page/src/App.jsx
+++ Landing page/src/App.jsx
@@ -6,6 +6,7 @@ import TechnicalCards from './components/TechnicalCards'
 import TechnicalSection from './components/TechnicalSection'
 import TestimonialSection from './components/TestimonialSection'
 
+
 const App = () => {
   return (
     <>
@@ -14,6 +15,7 @@ const App = () => {
      <ProjectSection />
      <TechnicalSection />
      <TestimonialSection />
+     
     </>
   )
 }
--- Landing page/src/components/TestimonialSection.jsx
+++ Landing page/src/components/TestimonialSection.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import data from '../Data/testimonial.json'
 import TestimonialCard from './TestimonialCard'
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
 
 const TestimonialSection = () => {
   return (
@@ -12,15 +15,43 @@ const TestimonialSection = () => {
       </div>
 
       <div className="testimonialcardparent">
-        {data.map((testimonial, index) => (
-          <TestimonialCard
-            key={index}
-            name={testimonial.name}
-            quote={testimonial.quote}
-            role={testimonial.role}
-            image={testimonial.image}
-          />
-        ))}
+        <Swiper
+          slidesPerView={1}
+          loop={true}
+          speed={2000}
+          spaceBetween={60}
+          autoplay={{
+            delay: 1000,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: false,
+          }}
+          breakpoints={{
+            786: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 24,
+            },
+          }}
+          allowTouchMove={false}
+          grabCursor={false}
+          watchSlidesProgress={true}
+          resistanceRatio={0}
+          modules={[Autoplay]}
+        >
+          {data.map((testimonial, idx) => (
+            <SwiperSlide key={idx}>
+              <TestimonialCard
+                name={testimonial.name}
+                quote={testimonial.quote}
+                role={testimonial.role}
+                image={testimonial.image}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   )
